051693026384a6e8baa71463aff5e5a82502c745
km <k.muresource@gmail.com>
2020-04-13 17:17:04 +0900

    Initial Commit

---
+++ Swift5Git1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift5Git1
+//
+//  Created by km1987 on 2020/04/13.
+//  Copyright © 2020 km1987. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
